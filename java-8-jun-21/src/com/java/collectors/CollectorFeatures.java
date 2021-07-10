404761a2dc2d764036ef6f5a6f76517a38be1e92
Jagannath Sahu <jagannath8715@gmail.com>
2021-07-10 11:57:52 +0530

    using collectors

---
+++ java-8-jun-21/src/com/java/collectors/CollectorFeatures.java
@@ -0,0 +1,41 @@
+package com.java.collectors;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
+import com.java.collectors.Dish.Type;
+
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+import static com.java.collectors.Dish.menu;
+
+public class CollectorFeatures {
+
+	public static void main(String... args) {
+        System.out.println("Dishes grouped by type: " + groupDishesByType());
+        System.out.println("Most caloric Dish by type: " + mostCaloricDishByType());
+        Map<Type, Optional<Dish>> dishMap= mostCaloricDishByType1();
+        //dishMap.replaceAll((key, oldValue) -> Optional.empty());
+        Map<Type, Dish> dishMap1 = dishMap.entrySet().stream()
+        		.collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().get()));
+        System.out.println("Most caloric Dish by type: " + dishMap1);
+    }
+
+    private static Map<Dish.Type, List<Dish>> groupDishesByType() {
+        return menu.stream().collect(groupingBy(Dish::getType));
+    }
+
+    private static Map<Dish.Type, Dish> mostCaloricDishByType() {
+        //BinaryOperator<Dish> binOp = BinaryOperator.maxBy(comparingInt(Dish::getCalories));
+        return menu.stream().collect(groupingBy(Dish::getType,
+                collectingAndThen(
+                        reducing(BinaryOperator.maxBy(comparingInt(Dish::getCalories))), Optional::get)));
+    }
+
+    private static Map<Type, Optional<Dish>> mostCaloricDishByType1() {
+        return menu.stream().collect(groupingBy(Dish::getType,
+                maxBy(Comparator.comparing(Dish::getCalories))));
+
+    }
+}
