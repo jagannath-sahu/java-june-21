10aeccceace2c88c32849de53a756fea218ca740
Jagannath Sahu <jagannath8715@gmail.com>
2021-09-05 21:43:21 +0530

    Adding one more visitor class for adding class attribute in html tag

--- java-8-jun-21/src/com/javacodegeeks/patterns/visitorpattern/TestCompositePattern.java
+++ java-8-jun-21/src/com/javacodegeeks/patterns/visitorpattern/TestCompositePattern.java
@@ -4,6 +4,7 @@ public class TestCompositePattern {
 
 	public static void main(String[] args) {
 		HtmlTag parentTag = new HtmlParentElement("<html>");
+		HtmlTagVisitor cssClassVisitor = new CssClassVisitor();
 		parentTag.setStartTag("<html>");
 		parentTag.setEndTag("</html>");
 
@@ -17,18 +18,25 @@ public class TestCompositePattern {
 		child1.setStartTag("<p>");
 		child1.setEndTag("</p>");
 		child1.setTagBody("Testing html tag library");
+		child1.accept(cssClassVisitor);
 		p1.addChildTag(child1);
+		p1.accept(cssClassVisitor);
 
 		HtmlTag child2 = new HtmlElement("<p>");
 		child2.setStartTag("<p>");
 		child2.setEndTag("</p>");
 		child2.setTagBody("Paragraph 2");
+		child2.accept(cssClassVisitor);
 		p1.addChildTag(child2);
 
 		//parentTag.generateHtml();
 
+		parentTag.accept(cssClassVisitor);
+
 		HtmlTagVisitor visitor = new HtmlTagVisitorImpl();
 		parentTag.accept(visitor);
+
+
 
 	}
 
